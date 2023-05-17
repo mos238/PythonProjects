9e49df5b6d7c366a8b1a9a93d81172c3a11e3256
MOHAMMED SAYED <omarsayed@gmail.com>
2023-05-17 10:08:20 +0100

    commit

---
+++ venv/pencolor.py
@@ -0,0 +1,21 @@
+from turtle import *
+
+import blue as blue
+speed(1)
+pensize(20)
+pencolor('red'),
+forward(50)
+pencolor(0, 1.0, 0)
+forward(50)
+pencolor(0, 0.5, 0.5),
+forward(50)
+
+pensize(10 )
+goto(-400,50)
+
+for red in range(4):
+    for green in range(4):
+        for blue in range(4):
+            pencolor(red / 4.0, green / 4.0, blue / 4.0)
+            forward(10)
+done()
